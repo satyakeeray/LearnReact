05117ddf8e4df6080090ead38d23d3786acc0758
satyakeeray <97254733+satyakeeray@users.noreply.github.com>
2023-11-19 13:44:56 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React from "react";
-import "./style.css";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import './style.css';
+import Navbar from './components/Navbar';
 
 export default function App() {
   let a = 10;
   let b = 20;
   let c = a + b;
-  let user = ['user1','user2','user3','user4','user5'];
+  let user = ['user1', 'user2', 'user3', 'user4', 'user5'];
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Navbar/>
-      <p>The addition of {a} and {b} is : {c}</p>
-      <ul>
-        {
-          user.map(
-            (user) => {
-              return (
-                <li>{user}</li>
-              ) 
-            }
-          )
-        }
-      </ul>      
+      <header>
+        <h1>Learning React JS</h1>
+      </header>
+      {/* This is example of simple component. */}
+      <Navbar />
+
+      <section id="section1">
+        <h2>Number Operations</h2>
+        <p>a = 10;</p>
+        <p>b = 20;</p>
+        <p> c = a + b;</p>
+        <p>The addition of {a} amd {b} is: {c}.</p>
+      </section>
+
+      <section id="section2">
+        <h2>Section 2</h2>
+        <p>This is the content of Section 2.</p>
+      </section>
+
+      <section id="section3">
+        <h2>Section 3</h2>
+        <p>This is the content of Section 3.</p>
+      </section>
+
+      <footer>
+        <p>By: Satyakee Ray</p>
+      </footer>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 const Navbar = () => {
-  return <div>This is Navbar</div>;
+  return (
+    <nav>
+        <ul>
+            <li><a href="#section1">Number Operations</a></li>
+            <li><a href="#section2">Section 2</a></li>
+            <li><a href="#section3">Section 3</a></li>
+        </ul>
+    </nav>
+  );
 };
 export default Navbar;
